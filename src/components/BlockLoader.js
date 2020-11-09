9e6ae2c5006c37c26ba7ebf96a6793a699a4ad53
Musango Wope <musangow@gmail.com>
2020-11-09 21:13:45 +0200

    create generic UI BlockLoader component

---
+++ src/components/BlockLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlockLoader = () => (
+  <div className="block-loader">
+    <div className="block-loader__content">
+      <div className="block-loader__content__loader" />
+    </div>
+  </div>
+);
+
+export default BlockLoader;
